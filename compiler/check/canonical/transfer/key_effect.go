1c5edf9a424567fd4e96661f5ddefe00731719b9
Wolfy-J <wolfy.jd@gmail.com>
2026-06-06 01:38:01 -0400

    Move key domain reads into flow

--- compiler/check/canonical/transfer/key_effect.go
+++ compiler/check/canonical/transfer/key_effect.go
@@ -4,8 +4,6 @@ import (
 	"github.com/wippyai/go-lua/types/constraint"
 	"github.com/wippyai/go-lua/types/domain/value/product"
 	"github.com/wippyai/go-lua/types/flow"
-	"github.com/wippyai/go-lua/types/typ"
-	"github.com/wippyai/go-lua/types/typ/unwrap"
 )
 
 type KeyProvenanceKind uint8
@@ -81,7 +79,11 @@ func (t *Transfer) applyKeyProvenanceEffect(out *flow.PointState, effect KeyProv
 		var keyValue product.AbstractValue
 		tableKeys, changed := flow.ApplyIndexedKeyArrayIterationProof(out, arrayAddr, keyAddr)
 		for _, tableKey := range tableKeys {
-			if keyType, ok := t.keyDomainForPathKey(out, tableKey); ok {
+			tableAddr, ok := flow.StableAddressFromKey(tableKey)
+			if !ok {
+				continue
+			}
+			if keyType, ok := flow.PointFactsOf(*out).KeyDomainAtAddress(tableAddr); ok {
 				av := product.FromType(keyType)
 				if keyValue.IsZero() {
 					keyValue = av
@@ -101,63 +103,5 @@ func (t *Transfer) applyKeyProvenanceEffect(out *flow.PointState, effect KeyProv
 		return changed
 	default:
 		return false
-	}
-}
-
-func (t *Transfer) keyDomainForPathKey(out *flow.PointState, key constraint.PathKey) (typ.Type, bool) {
-	sym, segments, ok := flow.ParseSymbolPathKey(key)
-	if !ok {
-		return nil, false
-	}
-	base, ok := t.symbolValue(out, sym)
-	if !ok || base.IsZero() {
-		return nil, false
-	}
-	if len(segments) > 0 {
-		base, ok = productMemberPathValue(base, segments)
-		if !ok || base.IsZero() {
-			return nil, false
-		}
-	}
-	keyType := keyDomainFromProductValue(base)
-	return keyType, keyType != nil && !typ.IsAbsentOrUnknown(keyType)
-}
-
-func keyDomainFromProductValue(av product.AbstractValue) typ.Type {
-	if av.IsZero() {
-		return nil
-	}
-	return keyDomainFromType(av.ProjectValue())
-}
-
-func keyDomainFromType(t typ.Type) typ.Type {
-	switch v := unwrap.Alias(t).(type) {
-	case *typ.Map:
-		return v.Key
-	case *typ.ReadonlyMap:
-		return v.Key
-	case *typ.Record:
-		keys := make([]typ.Type, 0, len(v.Fields)+1)
-		if v.HasMapComponent() {
-			keys = append(keys, v.MapKey)
-		}
-		if v.Open && !v.HasMapComponent() {
-			keys = append(keys, typ.String)
-		} else {
-			for _, field := range v.Fields {
-				keys = append(keys, typ.LiteralString(field.Name))
-			}
-		}
-		return typ.NewUnion(keys...)
-	case *typ.Union:
-		keys := make([]typ.Type, 0, len(v.Members))
-		for _, member := range v.Members {
-			if key := keyDomainFromType(member); key != nil && !typ.IsAbsentOrUnknown(key) {
-				keys = append(keys, key)
-			}
-		}
-		return typ.NewUnion(keys...)
-	default:
-		return nil
 	}
 }
