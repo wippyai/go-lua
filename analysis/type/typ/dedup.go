f41ed1e0bdba36f8a5ad99624a255e6cf5551c4d
Wolfy-J <wolfy.jd@gmail.com>
2026-06-10 07:21:56 -0400

    Fold union helper files into owners

--- analysis/type/typ/dedup.go
+++ analysis/type/typ/dedup.go
@@ -1,5 +1,7 @@
 package typ
 
+import "sort"
+
 type hashedType struct {
 	typ  Type
 	hash uint64
@@ -34,6 +36,26 @@ func deduplicateTypesWithHashes(types []Type) ([]Type, []uint64) {
 	}
 
 	return result, hashes
+}
+
+func sortHashedTypes(types []Type, hashes []uint64) {
+	if len(types) != len(hashes) || len(types) < 2 {
+		return
+	}
+	slots := make([]hashedType, len(types))
+	for i, t := range types {
+		slots[i] = hashedType{typ: t, hash: hashes[i]}
+	}
+	sort.Slice(slots, func(i, j int) bool {
+		if slots[i].hash != slots[j].hash {
+			return slots[i].hash < slots[j].hash
+		}
+		return slots[i].typ.String() < slots[j].typ.String()
+	})
+	for i, slot := range slots {
+		types[i] = slot.typ
+		hashes[i] = slot.hash
+	}
 }
 
 // UnionMemberHash returns the hash paired with SameUnionMember for normalized
--- analysis/type/typ/union.go
+++ analysis/type/typ/union.go
@@ -124,3 +124,15 @@ func (u *Union) Hash() uint64 { return u.hash }
 func (u *Union) Equals(other Type) bool {
 	return TypeEquals(u, other)
 }
+
+// Contains checks if the union contains a specific type.
+func (u *Union) Contains(t Type) bool {
+	h := UnionMemberHash(t)
+	for _, m := range u.Members {
+		if UnionMemberHash(m) == h && SameUnionMember(m, t) {
+			return true
+		}
+	}
+
+	return false
+}
