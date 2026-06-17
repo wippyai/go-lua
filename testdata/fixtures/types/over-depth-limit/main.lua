226ae19c2a8f4b6002d66b286409b02d87570c18
Wolfy-J <wolfy.jd@gmail.com>
2026-06-17 19:36:11 -0400

    Harden: deep-but-finite type past recursion-depth boundary

---
+++ testdata/fixtures/types/over-depth-limit/main.lua
@@ -0,0 +1,4 @@
+type Deep = { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: { f: number } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } } }
+local function id(x: Deep): Deep return x end
+local d: Deep
+return d
