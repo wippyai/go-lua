32bed641e4daae19d036aa27510f34dc13d174a4
Wolfy-J <wolfy.jd@gmail.com>
2026-07-05 20:59:30 -0400

    Move static branch truthiness to branchcond

--- analysis/lua/transferfacts/branch_reachability.go
+++ analysis/lua/transferfacts/branch_reachability.go
@@ -4,7 +4,7 @@ import (
 	factflow "github.com/wippyai/go-lua/analysis/engine/factflow"
 	"github.com/wippyai/go-lua/analysis/ir/cfg"
 	"github.com/wippyai/go-lua/analysis/ir/wir"
-	"github.com/wippyai/go-lua/analysis/lua/sourceprovenance"
+	"github.com/wippyai/go-lua/analysis/lua/branchcond"
 	"github.com/wippyai/go-lua/compiler/ast"
 )
 
@@ -21,49 +21,9 @@ func (l *lowerer) branchEdgeReachability(point cfg.Point, expr ast.Expr) (factfl
 }
 
 func semanticBranchEdgeReachability(expr ast.Expr) (factflow.BranchEdgeReachability, bool) {
-	truthy, ok := staticLuaTruthiness(expr)
+	truthy, ok := branchcond.StaticLuaTruthiness(expr)
 	if !ok {
 		return factflow.BranchEdgeReachability{}, false
 	}
 	return factflow.NewBranchEdgeReachability(!truthy, truthy), true
-}
-
-func staticLuaTruthiness(expr ast.Expr) (bool, bool) {
-	switch expr := sourceprovenance.AssertionInner(expr).(type) {
-	case *ast.NilExpr, *ast.FalseExpr:
-		return false, true
-	case *ast.TrueExpr, *ast.NumberExpr, *ast.StringExpr, *ast.TableExpr, *ast.FunctionExpr:
-		return true, true
-	case *ast.UnaryNotOpExpr:
-		value, ok := staticLuaTruthiness(expr.Expr)
-		if !ok {
-			return false, false
-		}
-		return !value, true
-	case *ast.LogicalOpExpr:
-		return staticLogicalTruthiness(expr)
-	default:
-		return false, false
-	}
-}
-
-func staticLogicalTruthiness(expr *ast.LogicalOpExpr) (bool, bool) {
-	left, ok := staticLuaTruthiness(expr.Lhs)
-	if !ok {
-		return false, false
-	}
-	switch expr.Operator {
-	case "and":
-		if !left {
-			return false, true
-		}
-		return staticLuaTruthiness(expr.Rhs)
-	case "or":
-		if left {
-			return true, true
-		}
-		return staticLuaTruthiness(expr.Rhs)
-	default:
-		return false, false
-	}
 }
