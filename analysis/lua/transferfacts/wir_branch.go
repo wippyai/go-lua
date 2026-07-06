25ccf55df76dc56ccef026383f8fc68a41c52e18
Wolfy-J <wolfy.jd@gmail.com>
2026-07-06 05:10:43 -0400

    Delete dead semantic branch helper

--- analysis/lua/transferfacts/wir_branch.go
+++ analysis/lua/transferfacts/wir_branch.go
@@ -5,7 +5,6 @@ import (
 	"github.com/wippyai/go-lua/analysis/ir/cfg"
 	"github.com/wippyai/go-lua/analysis/ir/wir"
 	"github.com/wippyai/go-lua/analysis/lua/branchcond"
-	"github.com/wippyai/go-lua/analysis/lua/semantics"
 	"github.com/wippyai/go-lua/analysis/lua/sourceprovenance"
 )
 
@@ -52,20 +51,6 @@ func (l *lowerer) branchConditionSourceAtWIR(point cfg.Point) (factflow.ValueSou
 		return source, ok
 	}
 	return factflow.ValueSource{}, false
-}
-
-func (l *lowerer) semanticDirectBranchCheckAt(point cfg.Point, result *semantics.Result) (branchcond.Check, bool) {
-	if l != nil && l.wir != nil {
-		return branchcond.Check{}, false
-	}
-	if result == nil {
-		return branchcond.Check{}, false
-	}
-	fact, ok := result.BranchCondition(point)
-	if !ok || fact.Check.Kind == branchcond.CheckNone {
-		return branchcond.Check{}, false
-	}
-	return fact.Check, true
 }
 
 func (l *lowerer) firstDirectBranchCheckFromWIR(point cfg.Point) (branchcond.Check, bool) {
