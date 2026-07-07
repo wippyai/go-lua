8830b2e7f0681b38dd01234c399400ae557ea9bc
Wolfy-J <wolfy.jd@gmail.com>
2026-07-07 12:22:26 -0400

    Delete dead branch alias fallback

--- analysis/lua/transferfacts/branch_condition_alias.go
+++ analysis/lua/transferfacts/branch_condition_alias.go
@@ -4,10 +4,7 @@ import (
 	factflow "github.com/wippyai/go-lua/analysis/engine/factflow"
 	"github.com/wippyai/go-lua/analysis/ir/cfg"
 	"github.com/wippyai/go-lua/analysis/lua/branchcond"
-	"github.com/wippyai/go-lua/analysis/lua/pathexpr"
-	"github.com/wippyai/go-lua/analysis/lua/sourceprovenance"
 	"github.com/wippyai/go-lua/analysis/symbol"
-	"github.com/wippyai/go-lua/compiler/ast"
 )
 
 func (l *lowerer) addLocalConditionAlias(sym symbol.ID, source factflow.ValueSource) {
@@ -22,30 +19,6 @@ func (l *lowerer) addLocalConditionAlias(sym symbol.ID, source factflow.ValueSou
 		l.localConditionAliases = make(map[symbol.ID]factflow.ExpressionCondition)
 	}
 	l.localConditionAliases[sym] = condition
-}
-
-func (l *lowerer) branchAliasRefinements(condition ast.Expr) []factflow.BranchRefinement {
-	aliased, trueValue, ok := l.aliasedExpressionCondition(condition)
-	if !ok {
-		return nil
-	}
-	return aliased.BranchRefinementsForValue(trueValue)
-}
-
-func (l *lowerer) branchAliasPathRelations(condition ast.Expr) []factflow.BranchPathRelation {
-	aliased, trueValue, ok := l.aliasedExpressionCondition(condition)
-	if !ok {
-		return nil
-	}
-	return aliased.BranchPathRelationsForValue(trueValue)
-}
-
-func (l *lowerer) branchAliasPathEvidence(condition ast.Expr) []factflow.BranchPathEvidence {
-	aliased, trueValue, ok := l.aliasedExpressionCondition(condition)
-	if !ok {
-		return nil
-	}
-	return aliased.BranchPathEvidenceForValue(trueValue)
 }
 
 func (l *lowerer) branchAliasRefinementsFromWIR(point cfg.Point) []factflow.BranchRefinement {
@@ -98,39 +71,5 @@ func (l *lowerer) wirAliasedExpressionCondition(check branchcond.Check) (factflo
 		return factflow.ExpressionCondition{}, false, false
 	}
 	condition, ok := l.localConditionAliases[check.Path.Symbol]
-	return condition, trueValue, ok
-}
-
-func (l *lowerer) aliasedExpressionCondition(expr ast.Expr) (factflow.ExpressionCondition, bool, bool) {
-	if expr == nil || l.bindings == nil {
-		return factflow.ExpressionCondition{}, false, false
-	}
-	trueValue := true
-	for {
-		not, ok := expr.(*ast.UnaryNotOpExpr)
-		if !ok || not == nil {
-			break
-		}
-		trueValue = !trueValue
-		expr = not.Expr
-	}
-	p, ok := pathexpr.Resolve(expr, l.bindings)
-	if !ok || p.Symbol == 0 || len(p.Segments) != 0 {
-		return factflow.ExpressionCondition{}, false, false
-	}
-	if l.bindings.HasWrite(p.Symbol) {
-		return factflow.ExpressionCondition{}, false, false
-	}
-	origin, ok := l.bindings.LocalOrigin(p.Symbol)
-	if !ok || origin.Stmt == nil || origin.Index < 0 || origin.Index >= len(origin.Stmt.Exprs) {
-		return factflow.ExpressionCondition{}, false, false
-	}
-	originExpr := origin.Stmt.Exprs[origin.Index]
-	source := sourceprovenance.SourceForExpr(originExpr, sourceprovenance.NoSourceIndex, sourceprovenance.NoSourceIndex, 0, true, false, l.callPointForExpr)
-	originRef := l.valueSource(source)
-	if !originRef.HasExpr {
-		return factflow.ExpressionCondition{}, false, false
-	}
-	condition, ok := l.expressionConditions[originRef.ExprRef]
 	return condition, trueValue, ok
 }
