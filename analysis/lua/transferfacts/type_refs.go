dab36535086277b0535c397f2717dc31c103ce4a
Wolfy-J <wolfy.jd@gmail.com>
2026-07-08 11:48:45 -0400

    Delete AST type ref adapter

--- analysis/lua/transferfacts/type_refs.go
+++ analysis/lua/transferfacts/type_refs.go
@@ -2,21 +2,7 @@ package transferfacts
 
 import (
 	"github.com/wippyai/go-lua/analysis/engine/factflow"
-	"github.com/wippyai/go-lua/compiler/ast"
 )
-
-func (l *lowerer) typeRefs(types []ast.TypeExpr) []factflow.TypeRef {
-	if len(types) == 0 {
-		return nil
-	}
-	out := make([]factflow.TypeRef, 0, len(types))
-	for i := range types {
-		if ref, ok := l.typeRef(types[i]); ok {
-			out = append(out, ref)
-		}
-	}
-	return out
-}
 
 func (l *lowerer) typeRef(typ any) (factflow.TypeRef, bool) {
 	if typ == nil {
