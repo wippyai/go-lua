67cd6eb62c490ff9be0955e0ae48073a02785423
Wolfy-J <wolfy.jd@gmail.com>
2026-07-22 10:44:20 -0400

    deletion: remove dead product validation helper

--- analysis/domain/value/product/runtime_validation.go
+++ analysis/domain/value/product/runtime_validation.go
@@ -11,14 +11,3 @@ func (rt *registryRuntime) validateValue(v Value) {
 		_ = rt.axisOrdinal(slot.ordinal)
 	}
 }
-
-func (rt *registryRuntime) axisValue(info axisRuntimeAxis, v Value) any {
-	if v.n != nil {
-		for _, slot := range v.n.slots {
-			if slot.ordinal == info.ordinal {
-				return slot.value
-			}
-		}
-	}
-	return info.topAny
-}
